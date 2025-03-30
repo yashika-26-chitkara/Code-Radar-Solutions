b4a26e3a9a8f7bb69882a85f1ed927902017901b
Yashika <yashikapahuja9@gmail.com>
2025-03-30 22:02:16 +0530

    Solution to Factorial Range

--- Factorial Range@@67de00956ac3fec9afb90e10/code.c
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -1,18 +1,15 @@
-int factorialRange(int num){
+int factorialRange(int start,int end){
     int start;
     scanf("%d",&start);
     int end;
     scanf("%d",&end);
-    int num;
     for(int i=start;i<=end;i++){
         if(start>end || start<0){
             return 0;
         }
-        return 1;
+        return num*factorialRange(num-1);
     }
     if(factorialRange(start,end)){
-        printf("%d\n",num*factorialRange(num-1));
-    }else{
-        printf("Invalid Range");
+        printf("")
     }
 }
